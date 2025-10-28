9803d2582699f76ca340e43f3b6468c90a1bcb62
Nikhil2005Rai <n2005rai@gmail.com>
2025-10-28 16:54:04 +0530

    Create template for ejs

--- app.js
+++ app.js
@@ -3,21 +3,33 @@ const express = require('express');
 //express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
+//giving path of view's
+// app.set('views', 'views'); //giving relative folder name (views by default)
+
 //listen for request
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname });
+    // res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname });
+    // res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about');
 });
 
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+// app.get('/about-us', (req, res) => {
+//     res.redirect('/about');
+// });
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
 });
 
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404');
 });
